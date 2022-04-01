834572b872f486a623e6096fff6ef50900e1ab68
Abhijit Tripathy <abhijittripathy.projects@gmail.com>
2022-04-01 00:41:09 -0400

    Change dataflow implementation

--- dataflow.h
+++ dataflow.h
@@ -19,73 +19,81 @@
 #include <stack>
 #include <stdio.h>
 #include <vector>
+  
+using namespace std;
 
 namespace llvm {
-
-enum passDirection {
-  FORWARD = 0,
-  BACKWARD = 1,
-};
-
-enum meetOperator {
-  UNION = 0,
-  INTERSECTION = 1,
-};
-
-enum blockType {
-  ENTRY = 0,
-  EXIT = 1,
-  REGULAR = 2,
-};
-
-struct basicBlockDeps {
-  BasicBlock *blockRef;
-  BitVector genSet;
-  BitVector killSet;
-};
-
-struct basicBlockProps {
-  enum blockType bType;
-  BasicBlock *block;
-  BitVector bbInput;
-  BitVector bbOutput;
-  BitVector genSet;
-  BitVector killSet;
-  std::vector<BasicBlock *> predBlocks;
-  std::vector<BasicBlock *> succBlocks;
-};
-
-class dataFlow {
-private:
-  int domainSize;
-  BitVector boundaryConditions;
-  BitVector initConditions;
-  enum meetOperator meetOp;
-  enum passDirection passDir;
-  std::vector<BasicBlock *> poTraversal;
-  std::vector<BasicBlock *> rpoTraversal;
-  void buildBasicBlockInfo(Function &F);
-  void genTraversalOrder(Function &F);
-  void getConnectedBlocks(BasicBlock *BB, struct basicBlockProps *basicBlock);
-  void applyTransferFunction(struct basicBlockProps *basicBlock);
-  void populateDepSets(std::map<BasicBlock *, basicBlockDeps *> bbSets);
-
-public:
-  std::map<BasicBlock *, basicBlockProps *> dataFlowHash;
-  dataFlow(int ds, enum meetOperator m, enum passDirection p, BitVector b,
-           BitVector i) {
-    domainSize = ds;
-    meetOp = m;
-    passDir = p;
-    boundaryConditions = b;
-    initConditions = i;
+  
+  enum pass_direction {
+    FORWARD = 0,
+    BACKWARD = 0
   }
-  BitVector calculateMeet(std::vector<BitVector> inputVectors);
-  void executeDataFlowPass(Function &F,
-                           std::map<BasicBlock *, basicBlockDeps *> bbSets);
-  void printMapping();
-};
 
-} // namespace llvm
+
+  enum meet {
+    UNION = 0,
+    INTERSECTION = 0
+  }
+
+  enum block_type {
+    ENTRY = 0,
+    EXIT = 1,
+    REGULAR = 2,
+  };
+
+  struct bb_req {           // struct for each BasicBlock's gen and kill sets that are required as inputs for dataflow analysis
+    BasicBlock *block_ref;
+    BitVector gen_set;
+    BitVector kill_set;
+  };
+
+  struct bb_props {
+    enum block_type b_t;
+    BasicBlock *block_ref;
+    BitVector bb_input;
+    BitVector bb_output;
+    BitVector gen_set;        // Gen Set for each block
+    BitVector kill_set;       // Kill set for each block
+    vector<BasicBlock *> p_blocks;        // Predecessor Blocks
+    vector<BasicBlock *> s_blocks;        // Successor Blocks
+  };
+
+
+  class Dataflow {
+    private:
+      int domain_size;
+      enum pass_direction dir;
+      map<BasicBlock*, struct bb_req*> bb_map;
+      BitVector (*meet_func)(vector<BitVector>);
+      void (*transfer_func)(struct bb_props*);
+      BitVector init_cond;
+      BitVector boundary_cond;
+      vector<BasicBlock *> po_traversal;
+      vector<BasicBlock *> rpo_traversal;
+      void init_props(Function &F);
+      void init_prev_and_next(BasicBlock *BB, struct bb_props *props);
+      void gen_traversal(Function &F);
+
+    public:
+      map<BasicBlock*, bb_props*> dfa;          // Dataflow Analysis result map
+      Dataflow(int domain_size,
+                enum pass_direction dir,
+                BitVector (*meet_func)(vector<BitVector>),
+                void (*transfer_func)(struct bb_props*),
+                map<BasicBlock*, struct bb_req*> bb_map, 
+                BitVector init_cond, 
+                BitVector boundary_cond) {        // Initialize the DFA
+
+        this->domain_size = domain_size;
+        this->dir = dir;
+        this->meet_fn = meet_fn;
+        this->transfer_fn = transfer_fn;
+        this->bb_map = bb_map;
+        this->init_cond = init_cond;
+        this->boundary_cond = boundary_cond;
+      }
+      void run(Function &F);
+  };
+}
 
 #endif
